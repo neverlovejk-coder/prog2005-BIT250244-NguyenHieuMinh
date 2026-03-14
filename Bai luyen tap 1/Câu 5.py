56cdde42fe839a41d3479dab723ac1e693b7e736
Nguyen Hieu Minh <neverlovejk@gmail.com>
2026-03-14 22:58:23 +0700

    Add files via upload

--- Bai luyen tap 1/Câu 5.py
+++ Bai luyen tap 1/Câu 5.py
@@ -4,7 +4,7 @@ N = int(input("nhap N: "))
 matrix = [[random.randint(1, 100) for _ in range(N)] for _ in range(M)]
 for row in matrix:
     print(row)
-r = int(input(f"nhap hang muon xem : "))
+r = int(input(f"nphap hang muon xem : "))
 print(f"hang {r}: {matrix[r]}")
 c = int(input(f"nhap cot muon xem: "))
 col_data = [matrix[i][c] for i in range(M)]
