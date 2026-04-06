abfe68f12d85f46a4da7098f8580a519681586d4
Nguyen Hieu Minh <neverlovejk@gmail.com>
2026-04-06 14:29:37 +0700

    Delete Bai Tap directory

--- Bai Tap/1.py
+++
@@ -1,4 +0,0 @@
-R = int(input("ban kinh = "))
-Pi = (3.14)
-chu_vi = 2 * Pi * R
-print(chu_vi)
--- Bai Tap/3.py
+++
@@ -1,4 +0,0 @@
-class Book:
-    def __init__(self,name,author,price):
-        self.name=name
-        self.author=price
