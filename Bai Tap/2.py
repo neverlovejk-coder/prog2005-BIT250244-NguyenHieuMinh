22f9bd67bb676c6937bc4f6f1337925c5499d690
Nguyen Hieu Minh <neverlovejk@gmail.com>
2026-04-06 14:27:15 +0700

    Add files via upload

---
+++ Bai Tap/2.py
@@ -0,0 +1,8 @@
+a = int(input("nhap so a= "))
+b = int(input("nhap so b= "))
+tong = a + b
+hieu = a - b
+tich = a * b
+print("tong 2 so la=",tong)
+print("hieu 2 so la=",hieu)
+print("tich 2 so la=",tich)
---
+++ Bai Tap/3.py
@@ -0,0 +1,4 @@
+class Book:
+    def __init__(self,name,author,price):
+        self.name=name
+        self.author=price
